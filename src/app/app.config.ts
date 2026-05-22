ff3702a2fb0b6c29034487a03a6642cd99931bc1
TheCap213 <jyonel295@gmail.com>
2026-05-22 08:17:49 -0500

    feat(iam): add authentication layout and sign-in structure.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,52 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { provideIcons } from '@ng-icons/core';
+import {
+  heroEnvelope,
+  heroLockClosed,
+  heroHeart,
+  heroEye,
+  heroEyeSlash,
+  heroUser,
+  heroBuildingOffice,
+  heroShieldCheck
+} from '@ng-icons/heroicons/outline';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+
+    provideRouter(routes),
+
+    provideHttpClient(withFetch()),
+
+    provideAnimationsAsync(),
+
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'es'
+    }),
+
+    provideIcons({
+      heroEnvelope,
+      heroLockClosed,
+      heroHeart,
+      heroEye,
+      heroEyeSlash,
+      heroUser,
+      heroBuildingOffice,
+      heroShieldCheck
+    })
   ]
 };
---
+++ src/app/iam/presentation/views/accept-invitation/accept-invitation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accept-invitation',
+  imports: [],
+  templateUrl: './accept-invitation.html',
+  styleUrl: './accept-invitation.css',
+})
+export class AcceptInvitation {}
